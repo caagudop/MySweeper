4c4542a6d149458065691adba6bc1fac9017dd2d
Carlos Agudo Postigo <carlosagudopos@gmail.com>
2020-06-22 09:42:10 +0200

    inicio proyecto;

--- MySweeper.py
+++ MySweeper.py
@@ -1,1 +1,9 @@
+import random, time, copy
+from termcolor import cprint
+
+print()
+cprint('Welcome to MineSweeper v.3.0!', 'red')
+cprint('=============================', 'red')
+print()
+print('Excited to declare version 3.0 of MineSweeper as almost fully functional!')
 
